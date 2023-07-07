937eb4b6fd249b64b72222c2a50eb5516cc355db
Sebastian <serranosebastian23@gmail.com>
2023-07-06 18:05:58 -0700

    minor changes

--- repos.py
+++ repos.py
@@ -10,15 +10,24 @@ print(r)
 
 # Store API response in a variable 
 response_dict = r.json() 
-print(f"Total repos: {response_dict['total_count']}") # 1
+print(f"Total repos: {response_dict['total_count']}")
 
 # explore information about the repos
-repo_dicts = response_dict['items'] # 2 
+repo_dicts = response_dict['items'] 
 print(f"Repos returned: {len(repo_dicts)}") 
-# examine the first repo
-repo_dict = repo_dicts[0] # 3
-print(f"\nKeys: {len(repo_dict)}") # 4 
-for key in sorted(repo_dict.keys()): # 5 
-    print(key)
+
+print(f"\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}") 
+    print(f"Owner: {repo_dict['owner']['login']}") 
+    print(f"Stars: {repo_dict['stargazers_count']}") 
+    print(f"Repos: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}") 
+    print(f"Updated: {repo_dict['updated_at']}") 
+    print(f"Description: {repo_dict['description']}")
+
+
+
+
 
 
